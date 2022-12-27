27c4eb6b6b908e4f34036ae1c906e69982343abe
Gui Ducci <84289159+gui-ducci@users.noreply.github.com>
2022-12-27 17:23:21 -0300

    Add files via upload

---
+++ minha_aposta.py
@@ -0,0 +1,96 @@
+import pandas as pd
+
+def numeros_sorteados():
+    # Lê a planilha inteira com a função read_excel()
+    df = pd.read_excel('numeros_sorteados.xlsx')
+
+    # Seleciona uma linha aleatória da planilha com a função sample()
+    linha_aleatoria = df.sample(1)
+
+    # Armazena os valores da linha selecionada em uma lista
+    combinacao = linha_aleatoria[0:35].values.tolist()[0]
+
+    # Retorna a combinação selecionada
+    return combinacao
+
+# Lê uma combinação aleatória de números sorteados da planilha
+combinacao = numeros_sorteados()
+
+# Imprime a combinação selecionada
+print(combinacao)
+
+def numeros_sorteados():
+    # Lê a planilha inteira com a função read_excel()
+    df = pd.read_excel('numeros_sorteados.xlsx')
+
+    # Seleciona uma linha aleatória da planilha com a função sample()
+    linha_aleatoria = df.sample(1)
+
+    # Armazena os valores da linha selecionada em uma lista
+    combinacao = linha_aleatoria[0:25].values.tolist()[0]
+
+    # Retorna a combinação selecionada
+    return combinacao
+
+# Lê uma combinação aleatória de números sorteados da planilha
+combinacao = numeros_sorteados()
+
+# Imprime a combinação selecionada
+print(combinacao)
+
+def numeros_sorteados():
+    # Lê a planilha inteira com a função read_excel()
+    df = pd.read_excel('numeros_sorteados.xlsx')
+
+    # Seleciona uma linha aleatória da planilha com a função sample()
+    linha_aleatoria = df.sample(1)
+
+    # Armazena os valores da linha selecionada em uma lista
+    combinacao = linha_aleatoria[0:18].values.tolist()[0]
+
+    # Retorna a combinação selecionada
+    return combinacao
+
+# Lê uma combinação aleatória de números sorteados da planilha
+combinacao = numeros_sorteados()
+
+# Imprime a combinação selecionada
+print(combinacao)
+
+def numeros_sorteados():
+    # Lê a planilha inteira com a função read_excel()
+    df = pd.read_excel('numeros_sorteados.xlsx')
+
+    # Seleciona uma linha aleatória da planilha com a função sample()
+    linha_aleatoria = df.sample(1)
+
+    # Armazena os valores da linha selecionada em uma lista
+    combinacao = linha_aleatoria[0:37].values.tolist()[0]
+
+    # Retorna a combinação selecionada
+    return combinacao
+
+# Lê uma combinação aleatória de números sorteados da planilha
+combinacao = numeros_sorteados()
+
+# Imprime a combinação selecionada
+print(combinacao)
+
+def numeros_sorteados():
+    # Lê a planilha inteira com a função read_excel()
+    df = pd.read_excel('numeros_sorteados.xlsx')
+
+    # Seleciona uma linha aleatória da planilha com a função sample()
+    linha_aleatoria = df.sample(1)
+
+    # Armazena os valores da linha selecionada em uma lista
+    combinacao = linha_aleatoria[0:20].values.tolist()[0]
+
+    # Retorna a combinação selecionada
+    return combinacao
+
+# Lê uma combinação aleatória de números sorteados da planilha
+combinacao = numeros_sorteados()
+
+# Imprime a combinação selecionada
+print(combinacao)
